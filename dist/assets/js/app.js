94b8b9cad71e8897d67d3b4870da98db7b174af5
Kogut-Pavel <73947945+Kogut-Pavel@users.noreply.github.com>
2022-02-27 13:51:13 +0500

    Add files via upload

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,136 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){n(1),n(2),e.exports=n(3)},function(e,t,n){"use strict";var o=document.getElementById("sidebarToggle"),r=(document.getElementById("sidebar"),document.getElementById("page")),c=document.body;function i(){c.classList.remove("show-sidebar"),document.querySelector(".page__mask").remove()}o.addEventListener("click",(function(e){var t;c.classList.contains("show-sidebar")?i():((t=document.createElement("div")).classList.add("page__mask"),t.addEventListener("click",i),r.appendChild(t),c.classList.add("show-sidebar"))}))},function(e,t,n){"use strict";var o=document.querySelectorAll("[data-modal]"),r=document.body,c=document.querySelectorAll(".modal__close"),i=document.querySelectorAll(".modal");function a(e){e.querySelector(".modal__content").removeAttribute("style"),setTimeout((function(){e.classList.remove("show"),r.classList.remove("no-scroll")}),200)}o.forEach((function(e){e.addEventListener("click",(function(e){var t=e.currentTarget.getAttribute("data-modal"),n=document.getElementById(t),o=n.querySelector(".modal__content");o.addEventListener("click",(function(e){e.stopPropagation()})),n.classList.add("show"),r.classList.add("no-scroll"),setTimeout((function(){o.style.transform="none",o.style.opacity="1"}),1)}))})),c.forEach((function(e){e.addEventListener("click",(function(e){a(e.currentTarget.closest(".modal"))}))})),i.forEach((function(e){e.addEventListener("click",(function(e){a(e.currentTarget)}))}))},function(e,t,n){"use strict";document.querySelectorAll("[data-autoresize]").forEach((function(e){var t=e.offsetHeight;e.addEventListener("input",(function(e){var n=e.target;n.style.height=t+"px",n.style.height=n.scrollHeight+"px"}))}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/assets/js/mobile-nav.js":
+/*!*************************************!*\
+  !*** ./src/assets/js/mobile-nav.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\n\r\nconst burger = document.getElementById('sidebarToggle');\r\nconst sidebar = document.getElementById('sidebar');\r\nconst page = document.getElementById('page');\r\nconst body = document.body;\r\n\r\nfunction closeSidebar() {\r\n    body.classList.remove('show-sidebar');\r\n    document.querySelector('.page__mask').remove();\r\n}\r\n\r\nfunction showSidebar() {\r\n    let mask = document.createElement('div');\r\n    mask.classList.add('page__mask');\r\n    mask.addEventListener('click', closeSidebar);\r\n    page.appendChild(mask);\r\n    body.classList.add('show-sidebar');\r\n}\r\n\r\nburger.addEventListener('click', event => {\r\n    if (body.classList.contains('show-sidebar')) {\r\n        closeSidebar();\r\n    } else {\r\n        showSidebar();\r\n    }\r\n});\r\n\r\n\r\nconst hasSubnav = document.querySelectorAll('.nav--mobile .has-subnav');\r\nconst subnav = document.querySelector('.nav--mobile .subnav');\r\n\r\n\r\nhasSubnav.forEach(item => {\r\n    item.addEventListener('click', () => {  \r\n        subnav.classList.toggle('active');\r\n    });\r\n});\r\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack:///./src/assets/js/mobile-nav.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/modal.js":
+/*!********************************!*\
+  !*** ./src/assets/js/modal.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\n\r\nconst modalBtn = document.querySelectorAll('[data-modal]');\r\nconst body = document.body;\r\nconst modalClose = document.querySelectorAll('.modal__close');\r\nconst modal = document.querySelectorAll('.modal');\r\n\r\n// Добавляем слушатель на кнопки с модальными окнами и добавляем классы при клике\r\nmodalBtn.forEach(item => {\r\n    item.addEventListener('click', event => {\r\n        let target = event.currentTarget;\r\n        let modalId = target.getAttribute('data-modal');\r\n        let modal = document.getElementById(modalId);\r\n        let modalContent = modal.querySelector('.modal__content');\r\n\r\n        modalContent.addEventListener('click', event => {\r\n            event.stopPropagation();\r\n        });\r\n\r\n        modal.classList.add('show');\r\n        body.classList.add('no-scroll');\r\n\r\n        setTimeout(() => {\r\n            modalContent.style.transform = \"none\";\r\n            modalContent.style.opacity = \"1\";\r\n        }, 1);\r\n        \r\n    });\r\n});\r\n\r\n// Закрываем модальное окно, убирая классы\r\nfunction closeModal(modal) {\r\n    let modalContent = modal.querySelector('.modal__content');\r\n    modalContent.removeAttribute('style');\r\n    setTimeout(() => {\r\n        modal.classList.remove('show');\r\n        body.classList.remove('no-scroll');\r\n    }, 200);\r\n}\r\n\r\n\r\n// Добавляем слушатель на крестики в модальных окнах\r\nmodalClose.forEach(item => {\r\n    item.addEventListener('click', event => {\r\n        let currentModal = event.currentTarget.closest('.modal');\r\n        closeModal(currentModal);\r\n    });\r\n});\r\n\r\nmodal.forEach(item => {\r\n    item.addEventListener('click', event => {\r\n        let currentModal = event.currentTarget;\r\n        closeModal(currentModal);\r\n\r\n\r\n    });\r\n});\r\n\n\n//# sourceURL=webpack:///./src/assets/js/modal.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/textarea.js":
+/*!***********************************!*\
+  !*** ./src/assets/js/textarea.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\n\r\nconst textArea = document.querySelectorAll('[data-autoresize]');\r\n\r\ntextArea.forEach(item => {\r\n    let textAreaH = item.offsetHeight;\r\n    item.addEventListener('input', event => {\r\n        let target = event.target;\r\n\r\n        target.style.height = textAreaH + 'px';\r\n        target.style.height = target.scrollHeight + 'px';\r\n    });\r\n});\n\n//# sourceURL=webpack:///./src/assets/js/textarea.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!************************************************************************************************!*\
+  !*** multi ./src/assets/js/mobile-nav.js ./src/assets/js/modal.js ./src/assets/js/textarea.js ***!
+  \************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! C:\\Users\\User\\Desktop\\Projects\\Personal_blog\\src\\assets\\js\\mobile-nav.js */\"./src/assets/js/mobile-nav.js\");\n__webpack_require__(/*! C:\\Users\\User\\Desktop\\Projects\\Personal_blog\\src\\assets\\js\\modal.js */\"./src/assets/js/modal.js\");\nmodule.exports = __webpack_require__(/*! C:\\Users\\User\\Desktop\\Projects\\Personal_blog\\src\\assets\\js\\textarea.js */\"./src/assets/js/textarea.js\");\n\n\n//# sourceURL=webpack:///multi_./src/assets/js/mobile-nav.js_./src/assets/js/modal.js_./src/assets/js/textarea.js?");
+
+/***/ })
+
+/******/ });
